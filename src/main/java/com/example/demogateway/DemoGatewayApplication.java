8a3baa5e742a9702a2ae9409d0bf378941e40f34
Toshiaki Maki <tmaki@pivotal.io>
2018-08-04 20:28:55 +0900

    Bump to Spring Cloud Finchley.SR1

--- src/main/java/com/example/demogateway/DemoGatewayApplication.java
+++ src/main/java/com/example/demogateway/DemoGatewayApplication.java
@@ -2,18 +2,11 @@ package com.example.demogateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.gateway.handler.predicate.CloudFoundryRouteServiceRoutePredicateFactory;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class DemoGatewayApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoGatewayApplication.class, args);
-	}
-
-	@Bean
-	public CloudFoundryRouteServiceRoutePredicateFactory cloudFoundryRouteServicePredicateFactory() {
-		return new CloudFoundryRouteServiceRoutePredicateFactory();
 	}
 }
